45615c312d047e0336ba7221b06c59029ecbce28
Anton <gorizont102.k@mail.ru>
2025-01-08 22:58:12 +0300

    add app help 4

--- layout/help_comp/main_help_4.php
+++ layout/help_comp/main_help_4.php
@@ -5,53 +5,7 @@
     </div>
     <div class="row">
         <div class="col-2">
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-                $name = $_POST['name'];
-                $mediaType = $_POST['media_type'];
-                $filename = $_POST['filename'];
-                $caption = $_POST['Caption'];
-                $status = $_POST['Status'];
-
-                $tried = ($_POST['tried'] == 'yes');
-
-                if ($tried) {
-                    $validated = (!empty($name) && !empty($mediaType) && !empty($filename));
-                    if (!$validated) { ?>
-                        <p>Имя, тип файла и имя файла - обязательные поля. Заполните их для продложения</p>
-                    <?php }
-                }
-                if ($tried && $validated) {
-                    echo "<p>Элеимент был создан</p>";
-                }
-
-                function mediaSelected($type)
-                {
-                    global $mediaType;
-                    if ($mediaType == $type) {
-                        echo "Тип файла выбран!";
-                    } ?>
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                        Имя : <input type="text" name="name" value="{$name} ?>"></br>
-                        Статус : <input type="checkbox" name="status" value="active"
-                            <?php
-                            if (isset($status)) {
-                                if ($status == "active") {
-                                    echo "checked";
-                                }
-                            }
-                            ?>
-                            <span>
-                        Тип:
-                        </span> <select name="media_type">
-
-                        </select>
-                    </form>
-            <?php }
-            }
-
-            ?>
+            
         </div>
         <div class="col">
             <!-- AJAX PHP tech -->
@@ -76,9 +30,12 @@
             <p><b>Start typing a name in the input field below:</b></p>
             <form action="">
                 <label for="fname">First name:</label>
-                <input type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
+                <input class="form-control border-4 w-50" type="text" id="fname" name="fname" onkeyup="showHint(this.value)">
             </form>
             <p>Suggestions: <span id="txtHint"></span></p>
+
+            <button class="btn btn-outline-secondary" type="reset" name="reset"><a class="text-decoration-none text-dark" href="../pages/help_4.php">Сбросить</a></button>
+
         </div>
     </div>
 
