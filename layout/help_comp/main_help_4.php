38a8030e3165bcdf120917b2bb56859d16a79107
Anton <gorizont102.k@mail.ru>
2025-01-08 23:25:22 +0300

    добавил time zone in help4

--- layout/help_comp/main_help_4.php
+++ layout/help_comp/main_help_4.php
@@ -4,10 +4,37 @@
         <p class="fs-5 text-body-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quod aut aliquid nostrum vel esse labore fugit quasi, dignissimos molestias libero temporibus.</p>
     </div>
     <div class="row">
-        <div class="col-2">
-            
+        <div class="col-2 w-25">
+            <h3>Time Zone: </h3>
+            <?php
+            $tz_LDN = new DateTimeZone('Europe/London');
+            $tz_TYO = new DateTimeZone('Asia/Tokyo');
+            $location = $tz_LDN->getLocation();
+
+            $LDN = new DateTime('now', $tz_LDN);
+            $TYO = new DateTime('now', $tz_TYO);
+            $SYD = new DateTime('now', new DateTimeZone('Australia/Sydney'));
+            ?>
+            <p><b>LDN: <?php $LDN->format('g:i a'); ?></b>
+                (<span>Разница во времени</span><?= ($LDN->getOffset() / (60 * 60)) ?>)
+                <hr>
+                <b>TYO: <?= $TYO->format('g:i a') ?></b>
+                (<span>Разница во времени</span><?= ($TYO->getOffset() / (60 * 60)) ?>)
+                <hr>
+                <b>SYD: <?= $SYD->format('g:i a') ?></b>
+                (<span>
+                Разница во времени
+                </span><?= $SYD->getOffset() / (60 * 60) ?>)
+                <hr>
+            </p>
+            <h1>Head Office</h1>
+            <p><?= $tz_LDN->getName() ?><br>
+                <b>Longitude: </b><?php $location['longitude']?><br>
+                <b>Latitude: </b><?php $location['latitude']?>
+            </p>
         </div>
-        <div class="col">
+        <!-- col 2 -->
+        <div class="col w-75">
             <!-- AJAX PHP tech -->
             <!-- <script src="../js/txtHint.js"></script> -->
             <script>
