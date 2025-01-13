5e91f7d1427d67ddf774223ebe35af493d66672e
Anton <gorizont102.k@mail.ru>
2025-01-13 09:34:01 +0300

    обновил страницу help 5, добавли account saving number

--- classes/Account.php
+++ classes/Account.php
@@ -1,27 +1,31 @@
 <?php
-class Accaunt{
+class Accaunt
+{
     public $number;
     public $type;
     protected $balance;
 
-    public function __construct($number, $type, $balance = 0.00)
+    public function __construct( $number, $type, $balance = 0.00)
     {
         $this->number = $number;
         $this->type = $type;
         $this->balance = $balance;
     }
 
-    public function deposite($amount){
-        $this->balance+= $amount;
+    public function deposite($amount)
+    {
+        $this->balance += $amount;
         return $this->balance;
     }
 
-    public function withdraw($amount){
+    public function withdraw($amount)
+    {
         $this->balance -= $amount;
         return $this->balance;
     }
 
-    public function getBalance(){
+    public function getBalance()
+    {
         return $this->balance;
     }
 }
--- layout/help_comp/main_help_5.php
+++ layout/help_comp/main_help_5.php
@@ -63,12 +63,14 @@
                 public $type;
                 public $balance;
 
-                public function deposit($amount) {
+                public function deposit($amount)
+                {
                     $this->balance += $amount;
                     return $this->balance;
                 }
-                public function withdraw($amount) {
-                    $this->balance-= $amount;
+                public function withdraw($amount)
+                {
+                    $this->balance -= $amount;
                     return $this->balance;
                 }
             }
@@ -80,45 +82,58 @@
 
             ?>
             <h2>Neo Bank</h2>
-            <p>Email : <?= $customer->email?></p>
-            <p>Balance : <?= $account->balance?></p>
+            <p>Email : <?= $customer->email ?></p>
+            <p>Balance : <?= $account->balance ?></p>
             <h2>Deposit</h2>
-            <p>$<?= $account->deposit(50.00)?></p>
+            <p>$<?= $account->deposit(50.00) ?></p>
         </div>
         <div class="row">
             <div class="col mt-4 ">
-                <?php 
+                <?php
                 include '../classes/Account.php';
                 $checking = new Accaunt(43161176, 'Checking', 32.00);
                 $savings = new Accaunt(20148896, 'Savings', 756.00);
 
                 ?>
-            
+
                 <h2 class="text-center m-3">Account Balances</h2>
 
-                    <table id="table_account_balances" class="table mx-auto p-2 w-50 border border-black "  >
-                        <tr>
-                            <th >Date</th>
-                            <th><?= $checking->type?></th>
-                            <th><?= $savings->type?></th>
-                        </tr> 
-                        <tr>
-                            <td>23 June</td>
-                        <td>$<?= $checking->getBalance();?></td>
-                        <td>$<?= $savings->getBalance();?></td>
-                    </td>
+                <table id="table_account_balances" class="table mx-auto p-2 w-50 border border-black ">
+                    <tr>
+                        <th>Date</th>
+                        <th><?= $checking->type ?></th>
+                        <th><?= $savings->type ?></th>
+                    </tr>
+                    <tr>
+                        <td>23 June</td>
+                        <td>$<?= $checking->getBalance(); ?></td>
+                        <td>$<?= $savings->getBalance(); ?></td>
+                        </td>
                     <tr>
                         <td>24 June</td>
-                        <td>$<?= $checking->deposite(12.00);?></td>
-                        <td>$<?= $savings->withdraw(100.00);?></td>
+                        <td>$<?= $checking->deposite(12.00); ?></td>
+                        <td>$<?= $savings->withdraw(100.00); ?></td>
                     </tr>
                     <tr>
                         <td>25 June</td>
-                        <td>$<?= $checking->deposite(5.00);?></td>
-                        <td>$<?= $savings->withdraw(300.00);?></td>
+                        <td>$<?= $checking->deposite(5.00); ?></td>
+                        <td>$<?= $savings->withdraw(300.00); ?></td>
                     </tr>
-                    
+
                 </table>
+                <div class="bg bg-secondary rounded-3 mt-5 p-2 w-50">
+
+                    <?php
+                    $numbers = [
+                        'account_number' => 12345678,
+                        'routing_number' => 987654321
+                    ];
+                    $account = new Accaunt($numbers, 'Savings', 10.00);
+                    ?>
+                    <h2><?= $account->type ?> acount</h2>
+                    <p>Account: <?= $account->number['account_number'] ?></p>
+                    <p>Account: <?= $account->number['routing_number'] ?></p>
+                </div>
 
             </div>
         </div>
