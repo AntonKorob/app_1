a5dff5658d02083629eccff20c2ebea543ed4c36
Anton <gorizont102.k@mail.ru>
2025-01-20 20:40:27 +0300

    добавил кнопки Create New Atricles и Create New Categories

--- layout/main_admin.php
+++ layout/main_admin.php
@@ -11,8 +11,18 @@ foreach ($members as $member) {
 }
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container">
-        <a class="navbar-brand" href="../layout/categories.php"><h3>Categories</h3></a>        
-        <a class="navbar-brand" href="../layout/atricles.php"><h3>Atricles</h3></a>      
-  </div>
+    <div class="container">
+        <div class="d-flex">
+            <a class="navbar-brand" href="../layout/categories.php">
+                <h3>Categories</h3>
+            </a>
+            <button type="button" class="btn btn-outline-primary"><a href="../crud/create_categorie.php">Create New Categories</a></button>
+        </div>
+        <div class="d-flex">
+            <a class="navbar-brand" href="../layout/atricles.php">
+                <h3>Atricles</h3>
+            </a>
+            <button type="button" class="btn btn-outline-primary"><a href="../crud/create_atricle.php">Create New Atricles</a></button>
+        </div>
+    </div>
 </nav>
